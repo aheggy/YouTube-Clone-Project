b69a7da22b8f9bc66fa515f29cb862707723aa3e
Raymond <raymondjohnson@pursuit.org>
2023-05-31 14:11:22 -0400

    VideoIndex

---
+++ src/components/video-index/VideoIndex.js
@@ -0,0 +1,23 @@
+import "./VideoIndex.css";
+
+export default function VideoIndex() {
+  return (
+    <div className="container">
+      <ul className="video-index">
+        <li className="video-item">
+          <span>Video Image</span>
+          <span>video 1</span>
+        </li>
+        <li className="video-item">Video 2</li>
+        <li className="video-item">Video 3</li>
+        <li className="video-item">Video 4</li>
+        <li className="video-item">video 5</li>
+        <li className="video-item">Video 6</li>
+        <li className="video-item">Video 7</li>
+        <li className="video-item">Video 8</li>
+        <li className="video-item">Video 9</li>
+        <li className="video-item">Video 10</li>
+      </ul>
+    </div>
+  );
+}
